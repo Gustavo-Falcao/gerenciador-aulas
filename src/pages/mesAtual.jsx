5d2872449292fb37fbf011c0e6887c6e9349339a
Gustavo <duartegusfalcao@gmail.com>
2025-10-14 09:58:18 -0300

    Adicionado lógica para incrementar ou decrementar o valor total das aulas no mes dinamicamente.

--- src/pages/mesAtual.jsx
+++ src/pages/mesAtual.jsx
@@ -4,10 +4,14 @@ import { useRef, useState } from 'react';
 
 function MesAtual() {
     const [dias, setDias] = useState(gerarListaMes() || []) 
-    const total = useRef(0)
-    function toggle(id) {
+    const [total, setTotal] = useState(0)
+    const titulo = useRef(gerarTitulo())
+    
+    function toggle(id, isMarcado) {
         setDias(prev => prev.map((item) => item.id === id ? {... item, marcado: !item.marcado} : item))
+        setTotal(prev => isMarcado ? prev - 30 : prev + 30);
     }
+
     const listaUl = <ul className='checklist clean'>
         {dias.map((dia) => 
         <li key={dia.id} className={dias.length === (dias.indexOf(dia))+1 ? "check" : "check border-bottom"} >
@@ -15,27 +19,25 @@ function MesAtual() {
             type="checkbox" 
             id={dia.id}
             checked={dia.marcado}
-            onChange={() => toggle(dia.id)}
+            onChange={() => toggle(dia.id, dia.marcado)}
             />
         <label htmlFor={dia.id}>{dia.dataFormatada}</label>
-        <small className='badge'>{dia.marcado ? "Ok" : "Pendendte"}</small>
+        <small className={dia.marcado ? "ok" : "badge"}>{dia.marcado ? "Ok" : "Pendendte"}</small>
     </li>)}
     </ul> 
-    const titulo = gerarTitulo();
 
     return(
         <>
             <div className='conteudo'>
                 <div className='box-dias'>
-                    <h1>{titulo}</h1>
+                    <h1>{titulo.current}</h1>
                     {listaUl}
                 </div>
                 <div className='total'>
                     <span>Total:</span>
-                    <span>R$ 270,00</span>
+                    <span>R$ {total}</span>
                 </div>
             </div>
-            
         </>
     )
 }
